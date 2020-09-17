1ba0ca02bdfa17cc1c62f9b2a5b8efbcf46b94ef
jaywcjlove <398188662@qq.com>
2020-09-17 17:32:56 +0800

    chore: 更新文档参数。

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { NativeModules } from 'react-native';
+import { NativeModules, Platform } from 'react-native';
 
 export default class TransportLocation {
   /**
@@ -7,30 +7,40 @@ export default class TransportLocation {
    * @param {String} appSecurity 网络货运企业在 省平台申请的接 入密钥
    * @param {String} enterpriseSenderCode 网络货运企业在 省平台申请的唯 一标识代码
    * @param {String} environment 环境:“debug” 接入测试环境， “release”接入 正式环境。
-   * @param {String} shippingNoteNumber 运单号
-   * @param {String} serialNumber 分单号
-   * @param {String} startCountrySubdivisionCode 起点行政区划代码
-   * @param {String} endCountrySubdivisionCode 终点行政区划代码
    */
   static init(appId, appSecurity, enterpriseSenderCode, environment) {
     return NativeModules.RNTransportLocation.init(appId, appSecurity, enterpriseSenderCode, environment);
   }
   /**
    * 启用定位
-   * @param {*} shippingNoteInfos 运单信息数组
+   * @param {String} shippingNoteNumber 当 Android 的时候，运单号
+   * @param {Array} shippingNoteNumber 当 iOS 的时候，运单信息数组
+   * @param {String} serialNumber 分单号
+   * @param {String} startCountrySubdivisionCode 起点行政区划代码
+   * @param {String} endCountrySubdivisionCode 终点行政区划代码
+   * 
    */
   static start(shippingNoteNumber, serialNumber, startCountrySubdivisionCode, endCountrySubdivisionCode) {
-
-    // JavaOnlyArray a = JavaOnlyArray.of(param);
-    return NativeModules.RNTransportLocation.start(shippingNoteNumber, serialNumber, startCountrySubdivisionCode, endCountrySubdivisionCode);
+    if (Platform.OS === 'android') {
+      return NativeModules.RNTransportLocation.stop(shippingNoteNumber, serialNumber, startCountrySubdivisionCode, endCountrySubdivisionCode);
+    }
+    if (Platform.OS === 'ios') {
+      return NativeModules.RNTransportLocation.stop([shippingNoteNumber, serialNumber, startCountrySubdivisionCode, endCountrySubdivisionCode]);
+    }
   }
   /**
    * 结束定位
-   * @param {Array} shippingNoteInfos 运单信息数组
+   * @param {String} shippingNoteNumber 运单号
+   * @param {String} serialNumber 分单号
+   * @param {String} startCountrySubdivisionCode 起点行政区划代码
+   * @param {String} endCountrySubdivisionCode 终点行政区划代码
    */
   static stop(shippingNoteNumber, serialNumber, startCountrySubdivisionCode, endCountrySubdivisionCode) {
-
-    // JavaOnlyArray a = JavaOnlyArray.of(param);
-    return NativeModules.RNTransportLocation.stop(shippingNoteNumber, serialNumber, startCountrySubdivisionCode, endCountrySubdivisionCode);
+    if (Platform.OS === 'android') {
+      return NativeModules.RNTransportLocation.stop(shippingNoteNumber, serialNumber, startCountrySubdivisionCode, endCountrySubdivisionCode);
+    }
+    if (Platform.OS === 'ios') {
+      return NativeModules.RNTransportLocation.stop([shippingNoteNumber, serialNumber, startCountrySubdivisionCode, endCountrySubdivisionCode]);
+    }
   }
 }

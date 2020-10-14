610c133c4a45830f762294b694401e396c6f21d0
jaywcjlove <398188662@qq.com>
2020-10-14 21:00:09 +0800

    chore: Update example. #4

--- example/App.js
+++ example/App.js
@@ -6,7 +6,6 @@
 
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import AMapGeolocation from '@uiw/react-native-amap-geolocation';
 import TransportLocation from '@uiw/react-native-transport-location';
 
 export default class App extends Component {
@@ -16,47 +15,24 @@ export default class App extends Component {
   };
 
   async componentDidMount() {
-    let apiKey = ''
-    if (Platform.OS === 'ios') {
-      apiKey = '00b74444d56a1f9e036b608a52f0da33';
-    }
-    if (Platform.OS === 'android') {
-      apiKey = '2d36ea36f37fb156a691d27901db0897';
-    }
-
-    if (apiKey) {
-      AMapGeolocation.setApiKey(apiKey);
-    }
-
     try {
-      // iOS 指定所需的精度级别
-      // AMapGeolocation.setDesiredAccuracy(3);
-      // Android 指定所需的精度级别，可选设置，默认 高精度定位模式
-      // AMapGeolocation.setLocationMode(1);
-      // 定位是否返回逆地理信息
-      // AMapGeolocation.setLocatingWithReGeocode(true);
-      // 当设备可以正常联网时，还可以返回该定位点的对应的中国境内位置信息（包括：省、市、区/县以及详细地址）。
-      AMapGeolocation.addLocationListener((location) => {
-        console.log('返回定位信息', location);
-      });
-      AMapGeolocation.start();
-
       const init = await TransportLocation.init("com.zkdata.ptug.truck", "8a744876-b716-4f4f-af99-46975162fb8a", "3337057", "release")
-      console.log('~9999999,,,', init);
-      TransportLocation.start("HT-HZ52008030000962", "0000", "320116", "421022")
-      // TransportLocation.stop("HT-HZ52008030000962", "0000", "320116", "421022")
-      // console.log('~sdsd55555,,,')
+      if (init && init.message) {
+        this.setState({ message: init.message });
+      }
+      console.log('1--->', init);
+      const start = await TransportLocation.start("HT-HZ52008030000962", "0000", "320116", "421022");
+      console.log('2--->', start);
     } catch (error) {
-        console.log('--->', error)
+      console.log('error::3--->', error)
     }
   }
   render() {
-    // console.log('~~~~:::4441')
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>☆RNTransportLocation example☆</Text>
+        <Text style={styles.welcome}>☆RNTransportLocation Example☆</Text>
         <Text style={styles.instructions}>STATUS: {this.state.status}</Text>
-        <Text style={styles.welcome}>☆NATIVE CALLBACK MESSAGE☆</Text>
+        <Text style={styles.welcome}>☆返回状态☆</Text>
         <Text style={styles.instructions}>{this.state.message}</Text>
       </View>
     );
@@ -77,7 +53,7 @@ const styles = StyleSheet.create({
   },
   instructions: {
     textAlign: 'center',
-    color: '#333333',
+    color: 'green',
     marginBottom: 5,
   },
 });
